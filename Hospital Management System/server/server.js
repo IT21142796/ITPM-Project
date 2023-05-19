507771dcd74de4ffa4e8828705dadd0d7e0fb219
HimanthaD4 <87825907+IT21129476@users.noreply.github.com>
2023-05-19 14:03:40 +0530

    implement backend controller and model class of blood management funtion

--- Hospital Management System/server/server.js
+++ Hospital Management System/server/server.js
@@ -1,19 +1,38 @@
+//dependencies calling
 const express = require("express");
-const colors = require("colors");
-const dotenv = require("dotenv").config();
-const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require("./config/db");
-const port = process.env.PORT || 5000;
-
-connectDB();
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//port declair
+const PORT = process.env.PORT || 8080;
 
-app.use("/api/myAmbulances", require("./routes/myAmbulanceRoutes"));
+app.use(cors())
+app.use(bodyParser.json());
 
-app.use(errorHandler);
+const bloodrouter = require("./routes/bloodRouter");
+app.use("/blood", bloodrouter);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const stationaryrouter = require("./routes/stationaryRouter");
+app.use("/stationary", stationaryrouter);
+
+const pharmacyrouter = require("./routes/pharmacyRouter");
+app.use("/pharmacy", pharmacyrouter);
+
+//connect mongoose
+mongoose.connect(process.env.link , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+ });
+
+ const connection = mongoose.connection;
+ connection.once("open", () => {
+     console.log("MongoDB Connection Success!");
+ });
+
+
+ app.listen(PORT, ()=>{
+    console.log(`The server is running on PORT ${PORT}`)
+})
